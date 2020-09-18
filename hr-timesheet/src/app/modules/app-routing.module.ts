66413a8f4a8d6e2981425662b9cefc454fee2373
Tylyn Adams <tyadams73@gmail.com>
2020-09-18 08:09:31 -0600

    unit 04 routing basics

--- hr-timesheet/src/app/modules/app-routing.module.ts
+++ hr-timesheet/src/app/modules/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AnalyticsComponent } from './../components/analytics/analytics.component';
+import { TimesheetComponent } from './../components/timesheet/timesheet.component';
+import { DepartmentsComponent } from './../components/departments/departments.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '',   redirectTo: 'departments', pathMatch: 'full' },
+  { path: 'departments', component: DepartmentsComponent },
+  { path: 'timesheet', component: TimesheetComponent },
+  { path: 'analytics', component: AnalyticsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
