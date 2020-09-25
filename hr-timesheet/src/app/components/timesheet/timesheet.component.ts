be1126b4f2baeeb5a85062f675310d0e959b9432
Tylyn Adams <tyadams73@gmail.com>
2020-09-25 10:06:34 -0600

    unit 7.2

--- hr-timesheet/src/app/components/timesheet/timesheet.component.ts
+++ hr-timesheet/src/app/components/timesheet/timesheet.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { DepartmentsService } from './../../services/departments.service';
 import { Department } from './../../interfaces/department';
 import { FormControl } from '@angular/forms';
+import { Employee } from './../../interfaces/employee';
 
 @Component({
   selector: 'app-timesheet',
@@ -13,6 +14,8 @@ export class TimesheetComponent implements OnInit {
     departments: Department[];
     department: Department;
     employeeNameFC = new FormControl('');
+    employees: Employee[] = [];
+    employeeId = 0;
 
   constructor(
     private route: ActivatedRoute,
@@ -22,6 +25,21 @@ export class TimesheetComponent implements OnInit {
   ngOnInit(): void {
     this.departments = this.departmentsService.departments;
     this.department = this.departments.find(department => department.id === this.route.snapshot.params['id']);
+}
+
+addEmployee(): void {
+  if (this.employeeNameFC.value) {
+      this.employeeId++;
+
+      this.employees.push({
+          id: this.employeeId.toString(),
+          departmentId: this.department.id,
+          name: this.employeeNameFC.value,
+          payRate: Math.floor(Math.random() * 50) + 50,
+      });
+
+      this.employeeNameFC.setValue('');
+  }
 }
 
 }
