824e65a25161f03a61aed538785dd8d6dca5ee51
Tylyn Adams <tyadams73@gmail.com>
2020-09-25 16:46:25 -0600

    Unit 8.2

--- hr-timesheet/src/app/components/timesheet/timesheet.component.ts
+++ hr-timesheet/src/app/components/timesheet/timesheet.component.ts
@@ -63,5 +63,13 @@ nameValidator(): ValidatorFn {
       return error;
   };
 }
+  getTotalHours(employee: Employee): number {
+    return employee.monday + employee.tuesday + employee.wednesday
+      + employee.thursday + employee.friday + employee.saturday + employee.sunday;
+}
+
+deleteEmployee(index: number): void {
+  this.employees.splice(index, 1);
+}
 
 }
